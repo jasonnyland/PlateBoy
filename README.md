5c8152bac3daac43edf2c88cb9877ef5d5b4a7b2
jnyl42 <jnyl42@gmail.com>
2017-05-02 22:55:33 -0700

    Small changes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # PlateBoy
-## Calculator for Weightlifting
+### Calculator for Weightlifting
 
 PlateBoy calculates assorted plate arrangements for use in weightlifting.
 
@@ -7,5 +7,5 @@ It also finds various percentages of the final target weight for warm-up sets.
 
 PlateBoy code is a prototype for either an Arduino handheld unit, or a mobile phone app.
 
-## Sample Output:
+#### Sample Output:
 <img src="https://github.com/jnyl42/PlateBoy/blob/master/plateboy.png?raw=true">
--- plate_boy.py
+++ plate_boy.py
@@ -1,6 +1,6 @@
 bar_weight = 45
 plate_sizes = [45, 35, 25, 10, 5, 2.5]
-plate_inventory = [10, 10, 10, 10, 10, 10]
+# plate_inventory = [10, 10, 10, 10, 10, 10]
 percent_increments = [60, 80, 100]
 dashes = "-----------------------------------"
 
@@ -20,14 +20,16 @@ def get_arrangement(target, percent):
             plate_pairs[index] += 1  # add plates to the pair counter
             remainder -= (2 * amt)  # subtract weight of added pair from remainder
 
-    print(str(percent) + "% of target", str(target), "lbs =", str(adj_target),
+    print(str(percent) + "% of target", str(target), "lbs =", str(adj_target), # print percent of target wt
           "lbs (bar =", str(bar_weight), "lbs)\n")
 
-    for index,amt in enumerate(plate_sizes):
+    for index, amt in enumerate(plate_sizes): # print each plate count
         if plate_pairs[index] > 0:
             print(str(amt) + "lb plates:", str(plate_pairs[index]), "pair")
-    if remainder > 0:
-        print("\n(Arrangement is ", str(remainder), "lbs ("+str("{0:.1f}".format(remainder/adj_target*100)) +"%) short of intended weight.)")
+
+    if remainder > 0: # print remainder
+        print("\n(Arrangement is ", str(remainder), "lbs ("+str("{0:.1f}".format(remainder/adj_target*100)) +"%)",
+              "short of intended weight.)")
     print(dashes)
 
 target = get_target()
